805f13da92717e7933aaabb942b51624c9f69a3d
KEVIN-DEVV <kyevenkims@gmail.com>
2022-12-02 14:22:03 +0300

    add function

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
+
+//  If the speed is less than 70, it should print “Ok”. Otherwise, for every 5 km/s above the speed limit (70), it should give the driver one demerit point and print the total number of demerit points.
 function get_speed(){
     let speed = parseInt(prompt('Speed input'));
     let demerits=Math.floor((speed-70)/5)
     if(speed<70){
         console.log("OK")
     } 
-    else if(speed>70){
-        console.log(demerits)
-       console.log("License suspended")   
+
+    else if (demerits>12){
+        console.log("License suspended")
     }
 
 }
 get_speed()
 
+
+// function put your garde then print outs the grade
+// if your grade is more than 100 the it will print out invalid number
 function get_grade(){
     let grade = parseInt(prompt('Enter your grade')); 
-    if(grade>79){
+    if(grade>79 && grade<=100){
         alert('Your grade is :A');
     }
     else if(grade>60 && grade<=79){
@@ -28,6 +33,9 @@ function get_grade(){
     }
     else if(grade<=40){
         alert("your grade is :E")
+    }
+    else if (grade>100){
+        alert("invalid number")
     }
 }
 get_grade()
@@ -45,43 +53,64 @@ function paye(number){
 }
 paye()
 
+
+//this is a function that returns the value of the nhif
+// when 
+
 function nhif(number){
         if (number<6000){
-            return number-150
+            return (150)
         }
         else if(number>6000 && number<8000){
-            return (number-300)
+            return (300)
         }
         else if(number>8000 && number<12000){
-            return (number-400)
+            return (400)
         }
         else if(number>12000 && number<15000){
-            return (number-500)
+            return (500)
         }
         else if(number>15000 && number<20000){
-            return (number-600)
+            return (600)
         }
         else if(number>20000 && number<25000){
-            return (number-750)
+            return (750)
         }
         else if(number>25000 && number<30000){
-            return (number-850)
+            return (850)
         }
         else if(number>30000 && number<35000){
-            return (number-900)
+            return (900)
          }
          else if(number>35000 && number<40000){
-            return (number-950)
+            return (950)
          }
          else if(number>40000 && number<45000){
-            return (number-1000)
+            return (1000)
          }
          else if(number>45000 && number<50000){
-            return (number-1100)
+            return (1100)
          }
-         else if(number>50000 && number<55000){
-            return (number-1200)
+         else if(number>50000 && number<60000){
+            return (1200)
+         }
+         else if(number>60000 && number<70000){
+            return (1300)
+         }
+         else if(number>70000 && number<80000){
+            return (1400)
+         }
+         else if(number>80000 && number<90000){
+            return (1500)
+         }
+         else if(number>90000 && number<100000){
+            return (1600)
+         }
+         else if(number>100000){
+            return (1700)
          }
                               
          
 }
+
+nhif()
